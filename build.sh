a641dab3c60762fe1417089570b7373383d251f7
Matthew <mshang@live.com>
2016-04-13 20:09:57 -0700

    Change .o file to illume.o

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # nvcc $(find ./src/ -regex ".*\.\(c\|cu\)") -lpng -o ./main.out
-nvcc -arch=compute_52 -rdc=true $(find ./src/ -regex ".*\.\(c\|cu\)") -lpng -o main.o
+nvcc -arch=compute_52 -rdc=true $(find ./src/ -regex ".*\.\(c\|cu\)") -lpng -o illume.o
