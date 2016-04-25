441f07a5a7ac55aec7498440ab0f9c9a2d9020e2
Matthew <mshang@live.com>
2016-04-24 18:18:18 -0700

    Added timer to build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-# nvcc $(find ./src/ -regex ".*\.\(c\|cu\)") -lpng -o ./main.out
+start=$(date +%s%N)
 nvcc -arch=compute_52 -rdc=true $(find ./src/ -regex ".*\.\(c\|cu\)") -lpng -o illume.o
+tt=$((($(date +%s%N) - $start)/1000000000))
+echo "Build time: $tt seconds"
